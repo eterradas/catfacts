02aa0e344e59a20e69f4b0024970d8fa8042cd0e
dylanclark112 <dylanclark112@gmail.com>
2012-12-01 20:20:20 -0800

    Restructured as Django Project.

---
+++ CatfactsMsgAgent/factlistgenerator.py
@@ -0,0 +1,14 @@
+from random import choice,shuffle
+from os import path
+
+intro = ['Welcome to CatFacts! The coolest Way to learn about cats! Your first CatFact: Cats spend a third of their waking hours grooming themselves!'] 
+signoff = [' Luv, CatFacts!', ' Awesome!', ' TO STOP REPLY <STOP>', ' This is Catsfacts!', ' Meow!']
+
+def generate_cat_facts_message_sequence():
+	outlist =[]
+	with open(path.join(path.dirname(__file__), "catfacts.txt")) as file:
+		for line in file:
+			outlist.append(str(line.rstrip() + choice(signoff)))
+		shuffle(outlist)
+        return intro + outlist
+
