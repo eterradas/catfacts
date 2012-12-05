f4cca0d016151749d7269c2730cda38ce2d0b81b
dylanclark112 <dylanclark112@gmail.com>
2012-12-04 17:34:13 -0800

    switched to bootstrap, switched broker to django

--- catfacts/catfactsmsgagent/sendsms.py
+++ catfacts/catfactsmsgagent/sendsms.py
@@ -2,7 +2,7 @@ from celery import Celery
 from twilio.rest import TwilioRestClient
 import datetime
 
-celery = Celery('tasks', backend='amqp', broker='amqp://')
+celery = Celery('tasks', backend='amqp', broker='django://')
 
 @celery.task(name='CatfactsMsgAgent.sendsms.send_sms')
 def send_sms(sms_to, sms_from, sms_body):
--- catfacts/landing/forms.py
+++ catfacts/landing/forms.py
@@ -3,13 +3,13 @@ from django.contrib.localflavor.us.forms import USPhoneNumberField
 from landing.models import Recipient
 
 class RecipientForm(forms.Form):
-    number = USPhoneNumberField(label='Your number', widget=forms.TextInput(attrs={'placeholder': '510-555-5555', 'class':'phone-input'}))
+    number = USPhoneNumberField(label='Your number', widget=forms.TextInput(attrs={'placeholder': '(510) 555-0624', 'class':'phone-input input-block-level'}))
 
     def clean_number(self):
         number = self.cleaned_data['number']        
         try:
             number=Recipient.objects.get(number__iexact=number)
-            raise forms.ValidationError("That number is already registered to recieve CatFacts.")
+            raise forms.ValidationError("That number is already registered.")
         except Recipient.DoesNotExist:
             pass
         return number
