281d27d09043177c00fe2bacbde2b9bc0a313c0f
dylanclark112 <dylanclark112@gmail.com>
2012-12-04 19:40:35 -0800

    preparing to deploy to heroku, added requirements.txt

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python catfacts/catfacts/manage.py runserver 0.0.0.0:$PORT --noreload
--- catfacts/catfacts/settings.py
+++ catfacts/catfacts/settings.py
@@ -153,3 +153,6 @@ LOGGING = {
         },
     }
 }
+
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
