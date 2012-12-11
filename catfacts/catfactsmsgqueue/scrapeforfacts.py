ad2bfa9d6932cf111bcbb7894215ef65b832e334
dylanclark112 <dylanclark112@gmail.com>
2012-12-10 20:54:55 -0800

    cleaned up catfactsmsgqueue

--- catfacts/catfactsmsgqueue/scrapeforfacts.py
+++
@@ -1,7 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-r = requests.get('https://user.xmission.com/~emailbox/trivia.htm')
-soup = BeautifulSoup(r.text)
-factgen=(li.get_text() for li in soup.find_all('li') if len(li.get_text())<120)
-for fact in factgen:
-    print ' '.join(fact.split())
